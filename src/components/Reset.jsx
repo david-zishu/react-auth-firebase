9d6408258b960cf30720ac3d9cac104a8441ef7a
David Zishu <david.zishu6393@gmail.com>
2024-02-03 15:11:05 +0530

    add send reset password functionality using firebase

--- src/components/Reset.jsx
+++ src/components/Reset.jsx
@@ -1,8 +1,18 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
+import { auth, sendPasswordReset } from "../firebase";
 
 const Reset = () => {
   const [email, setEmail] = useState("");
+  const [user, loading] = useAuthState(auth);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (loading) return;
+    if (user) navigate("/dashboard");
+  }, [user, loading]);
 
   return (
     <div className="reset">
@@ -14,7 +24,9 @@ const Reset = () => {
           onChange={(e) => setEmail(e.target.value)}
           placeholder="E-mail Address"
         />
-        <button className="reset__btn">Send password reset email</button>
+        <button className="reset__btn" onClick={() => sendPasswordReset(email)}>
+          Send password reset email
+        </button>
 
         <div>
           Don&apos;t have an account? <Link to="/register">Register</Link> now.
